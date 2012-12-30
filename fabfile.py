0671395dd5617fea32dd2875e0014007b5674943
Arun <arun@ubuntu.ubuntu-domain>
2012-12-31 05:25:43 +0530

    i dont care anymore

--- fabfile.py
+++ fabfile.py
@@ -16,8 +16,8 @@ def host_type():
 
 def ycsb(workload,recordcount):
   stime=strftime("%H%M%S")[1:4];
-  cmd='cd /home/ubuntu/ycsb-0.1.4/ && bin/ycsb load voldemort -p bootstrap_urls=tcp://10.151.3.175:6666 -p recordcount='+recordcount+' -P workloads/'+ workload
-  cmd='cd /home/ubuntu/ycsb-0.1.4/ && bin/ycsb run voldemort -p bootstrap_urls=tcp://10.151.3.175:6666 -p recordcount='+recordcount+' -P workloads/'+ workload + '> voldemort.'+stime+'.res'
+  cmd='cd /home/ubuntu/ycsb-0.1.4/ && bin/ycsb load voldemort -p bootstrap_urls=tcp://10.151.3.175:6666 -p recordcount='+recordcount+' -P workloads/'+ cmd
+  workload='cd /home/ubuntu/ycsb-0.1.4/ && bin/ycsb run voldemort -p bootstrap_urls=tcp://10.151.3.175:6666 -p recordcount='+recordcount+' -P workloads/'+ workload + '> res/voldemort.'+stime+'.res'
   # cmd= 'cd /home/ubuntu/ycsb-0.1.4/ && bin/ycsb run mongodb -s -p workloads/'+ workload + ' -p mongodb.url=mongodb://10.152.163.208:27017 -p mongodb.database=ycsb -p mongodb.writeConcern=normal > mongo.'+stime+'.res'
   # cmd='cd /home/ubuntu/ycsb-0.1.4/ && bin/ycsb load cassandra-10 -P workloads/'+ workload + ' -p recordcount=10000  -p hosts=10.152.150.205 -threads 10 -s > filepath'+stime+'.res'
 # bin/ycsb run cassandra-10 -P workloads/workloada -p recordcount=10000 -p operationcount=10000 -threads 10 -s > filepath
