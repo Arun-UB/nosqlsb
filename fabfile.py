ac3040148a82905ca6ee4fe7890525e8107344c4
Arun <arun@ubuntu.ubuntu-domain>
2012-12-31 12:40:47 +0530

    i dont care anymore

--- fabfile.py
+++ fabfile.py
@@ -8,10 +8,10 @@ def ycsb(voldemort,workload,recordcount):
   #stime=strftime("%H%M%S")[1:4]
   #Voldemort
   # if len(voldemort)>0 :
-    cmd='cd /home/ubuntu/ycsb-0.1.4/ && bin/ycsb load voldemort -p bootstrap_urls=tcp://'+voldemort_ip+':6666 -target 1000 -p recordcount='+recordcount+' -P workloads/'+ workload
-    local(cmd)
-    cmd='cd /home/ubuntu/ycsb-0.1.4/ && bin/ycsb run voldemort -p bootstrap_urls=tcp://'+voldemort_ip+':6666 -target 1000 -p recordcount='+recordcount+' -P workloads/'+ workload + ' > res/voldemort.res'
-    local(cmd)
+  cmd='cd /home/ubuntu/ycsb-0.1.4/ && bin/ycsb load voldemort -p bootstrap_urls=tcp://'+voldemort_ip+':6666 -target 1000 -p recordcount='+recordcount+' -P workloads/'+ workload
+  local(cmd)
+  cmd='cd /home/ubuntu/ycsb-0.1.4/ && bin/ycsb run voldemort -p bootstrap_urls=tcp://'+voldemort_ip+':6666 -target 1000 -p recordcount='+recordcount+' -P workloads/'+ workload + ' > res/voldemort.res'
+  local(cmd)
 
   #MongoDB
   # if len(mongo)>0 : 
@@ -21,8 +21,8 @@ def ycsb(voldemort,workload,recordcount):
   #   local(cmd)
   #Cassandra
   # if len(cassandra)>0 : 
-    cmd='cd /home/ubuntu/ycsb-0.1.4/ && bin/ycsb load cassandra-10 -s -P workloads/'+ workload + ' -p recordcount=10000  -p hosts=10.80.77.61 -threads 10 -p recordcount='+recordcount+' > res/cassandra.res'
-    local(cmd)
+  cmd='cd /home/ubuntu/ycsb-0.1.4/ && bin/ycsb load cassandra-10 -s -P workloads/'+ workload + ' -p recordcount=10000  -p hosts=10.80.77.61 -threads 10 -p recordcount='+recordcount+' > res/cassandra.res'
+  local(cmd)
 # bin/ycsb run cassandra-10 -P workloads/workloada -p recordcount=10000 -p operationcount=10000 -threads 10 -s > filepath
  # ./bin/ycsb load cassandra-10 -s -P workloads/workloada -p hosts=10.80.77.61 -target 8000 -threads 16 -p recordcount=100000 > testcasa.dat
   
